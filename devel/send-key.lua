40cf4ae2a2fd954f1d566c54a1470d92b0d21acf
lethosor <lethosor@gmail.com>
2016-06-15 00:03:34 -0400

    Document devel/send-key

--- devel/send-key.lua
+++ devel/send-key.lua
@@ -1,4 +1,16 @@
--- send a key to the current screen
+-- send a key to the current screen or a parent
+--[====[
+
+devel/send-key
+==============
+Send a key to the current screen or a parent
+
+Usage:
+:`devel/send-key KEY_NAME`:     Send KEY_NAME
+:`devel/send-key KEY_NAME X`:   Send KEY_NAME to the screen `X` screens above
+                                the current screen
+
+]====]
 args = {...}
 key = df.interface_key[args[1]]
 if not key then qerror('Unrecognized key') end
