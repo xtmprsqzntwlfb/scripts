560ec5ff74eba748707444a6661a9e7744033a31
Josh Cooper <cooper.s.josh@outlook.com>
2017-12-18 21:42:51 -0800

    Update pimp-it.lua (major revision)
    New Functions:
    -isDwarfNamed               //Returns true if the DWARF has a name (nickname) this dwarf is treated as sacred unless '-clear* allnamed' is specified
    -isDwarfCustom              //Returns true if the DWARF has a custom_profession
    -isDwarfPimped              //Returns true if the DWARF has a job
    -isDwarfUnpimped            //Returns true if the DWARF is a drunk with no job
    -isDwarfCustomUnpimped      //Returns true if the DWARF is custom, but also probably a drunk
    -isDwarfProtected           //Returns true if the DWARF has a job, or is custom with protection
    -isDwarfUnprotected         //Returns true if the DWARF has no job and or is not custom with protection
        ~~~~~~~~
    New Script Arguments:
    -clear
    -clearfirst
    options:
    >   allcustom
    >   allpimped
    >   allunpimped
    >   allcustomunpimped
    >   allprotected
    >   allunprotected
    >   allnamed
    >   all
    Note:
    '-clear all' will reset all dwarves
    '-clearfirst all' will reset all dwarves except named
        ~~~~~~~~
    isValidProfession() modifications:
    -local current = math.floor(prof.cur) + increment
    ->this is a major change for the comparison (current < prof.max) -- because increment <= 1.0
    -boundaries for rounding up/down ratio limits
    ->limit still prefers to be a value of at least 1
        ~~~~~~~~
    isValidJob() modifications:
    -implemented dorf_jobs minimums
    -when pimped_count < 7 forces only jobs with unmet minimums to be valid
    -added the same check for once the 7 pimped-dwarf minimum has been met
    ->this is in addition to the regular check however, though it does run first
        ~~~~~~~~
    ApplyJob() modifications:
    -rewrote sections to enforce newly implemented job minimums
    -modified priority/tertiary profession increments to 0.66 and 0.22 respectively
    -added code to ensure dwf.profession2 is assigned a value
        ~~~~~~~~
    FindJob() modifications:
    -implemented clearfirst argument with all its glorious options
    ->see top of commit message
    -integrated isDwarfProtected & isDwarfPimped
    ->simplified FindJob's job counting for already pimped dwarves
        ~~~~~~~~
    ZeroDwarf() modifications:
    -integrated zeroed_count
    -now resets dwarf professions to 'DRUNK'
    -now resets dwarf nicknames (because there is built in protection to avoid overwriting named dwarves)
        ~~~~~~~~
    general changes:
    -added checks to ensure skill.ration >= 0
    -added 'pimped_count'
    -added 'zeroed_count'
    -rewrote argument parsing/script execution decisions
    -script prints a new line when it finishes
        ~~~~~~~~
    todo:
    -write help
    -force help to display when no arguments are given (to avoid fucking up an unsaved game)
    -implement optimize argument

--- pimp-it.lua
+++ pimp-it.lua
@@ -1,17 +1,31 @@
 local rng = require('plugins.cxxrandom')
 local utils = require('utils')
 local dorf_tables = dfhack.script_environment('dorf_tables')
-dorf_tables.ResetProfessionTable()
 local validArgs = utils.invert({
     'applyjob',
     'applyprofession',
     'applytype',
     'selected',
-    'cleardwarf',
-    'cleardwarves',
+    'clear', --selected --all --allnamed --allcustom --allpimped --allunpimped --allcustomunpimped --allprotected --allunprotected
+    'clearfirst',       --all --allnamed --allcustom --allpimped --allunpimped --allcustomunpimped --allprotected --allunprotected
+    'query',
+    'resetdata',
     'debug'
 })
 local args = utils.processArgs({...}, validArgs)
+if args.query and args.query ~= "" then
+    dorf_tables.Query(string.upper(args.query))
+    return
+end
+if args.resetdata or args.clear or args.clearfirst then
+    dorf_tables.ResetDataTables(true)
+end
+custom_dwarf_signals = {'_', 'c', 'j', 'p'}
+
+
+function GetChar(str,i)
+    return string.sub(str,i,i)
+end
 
 function DisplayTable(t,query,field)
     print('###########################')
@@ -107,17 +121,82 @@ function GetRandomAttribLevel()
     return nil
 end
 
+--Returns true if the DWARF has a name (nickname) this dwarf is treated as sacred unless '-clear* allnamed' is specified
+function isDwarfNamed(dwf)
+    return dwf.status.current_soul.name.nickname ~= ""
+end
+
+--Returns true if the DWARF has a custom_profession
+function isDwarfCustom(dwf)
+    if dwf.custom_profession ~= "" or isDwarfNamed(dwf) then
+        return true
+    end
+    return false
+end
+
+--Returns true if the DWARF has a job
+function isDwarfPimped(dwf)
+    local jobName = dwf.custom_profession
+    local job = dorf_tables.dorf_jobs[jobName]
+    if job then
+        return true
+    end
+    return false
+end
+
+--Returns true if the DWARF is a drunk with no job
+function isDwarfUnpimped(dwf)
+    return (not isDwarfPimped(dwf))
+end
+
+--Returns true if the DWARF is custom, but also probably a drunk
+function isDwarfCustomUnpimped(dwf)
+    if isDwarfCustom(dwf) and isDwarfUnpimped(dwf) then
+        return true
+    end
+    return false
+end
+
+--Returns true if the DWARF has a job, or is custom with protection
+function isDwarfProtected(dwf)
+    if isDwarfNamed(dwf) or isDwarfPimped(dwf) then
+        return true
+    end
+    if dwf.custom_profession ~= "" then
+        for _,signal in pairs(custom_dwarf_signals) do
+            if GetChar(dwf.custom_profession, 1) == signal then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+--Returns true if the DWARF has no job and or is not custom with protection
+function isDwarfUnprotected(dwf)
+    return (not isDwarfProtected(dwf))
+end
+
 function isValidProfession(profession, incr)
     local prof = dorf_tables.professions[profession]
     assert(df.profession[profession], "Invalid profession: " .. profession .. " (not a built-in profession, no id found)")
+    --Does it exist?
     if prof ~= nil and prof.cur ~= nil and prof.max ~= nil then
         if args.debug and tonumber(args.debug) >= 2 then print("prof.max = " .. prof.max, "prof.cur = " .. prof.cur) end
-        if (prof.cur + incr) <= prof.max then
+        --Excedes hard limit?
+        --A/B Test with math.floor(current)
+        --local current = prof.cur + incr
+        local current = math.floor(prof.cur) + incr
+        if current < prof.max then
             if prof.ratio ~= nil then
                 local limit = (prof.ratio * dwarf_count)
-                limit = limit < 1.0 and limit >= 0.5 and math.ceil(limit) or math.floor(limit)
+                limit = limit < 1.0 and limit >= 0.75 and math.ceil(limit) or limit
+                local decimal_value = limit - math.floor(limit)
+                --limit = decimal >= 0.66 ? ceil(limit) : floor(limit)
+                limit = decimal_value >= 0.88 and math.ceil(limit) or math.floor(limit)
                 if args.debug and tonumber(args.debug) >= 3 then print("prof.ratio = " .. prof.ratio, "dwarf_count = " .. dwarf_count, "limit = " .. limit) end
-                if (prof.cur + incr) <= limit then
+                --Excedes population ratio of this profession?
+                if current <= limit then
                     return true
                 else
                     --surpassed ratio
@@ -134,13 +213,23 @@ end
 
 function isValidJob(job) --job is a dorf_jobs.<job> table
     if job ~= nil and job.req ~= nil then
-        for i, prof in pairs(job.req) do
-            if args.debug and tonumber(args.debug) >= 4 then print("isValidJob() req loop it#" .. i .. " prof:" .. prof) end
-            if not isValidProfession(prof, 1) then
-                return false
+        if pimped_count < 7 then
+            if job.cur and job.min and job.cur[1] < job.min[1] then
+                return true
             end
+            return false
+        else
+            if job.cur and job.min and job.cur[1] < job.min[1] then
+                return true
+            end
+            for i, prof in pairs(job.req) do
+                if args.debug and tonumber(args.debug) >= 4 then print("isValidJob() req loop it#" .. i .. " prof:" .. prof) end
+                if not isValidProfession(prof, 1) then
+                    return false
+                end
+            end
+            return true
         end
-        return true
     end
     return false
 end
@@ -214,12 +303,14 @@ function ApplyType(dwf, dorf_type)
         for skill, skillRange in pairs(type.skills) do
             local sTable = GetSkillTable(dwf, skill)
             if sTable == nil then
+                --print("ApplyType()", skill, skillRange)
                 utils.insert_or_update(dwf.status.current_soul.skills, { new = true, id = df.job_skill[skill], rating = 0 }, 'id')
                 sTable = GetSkillTable(dwf, skill)
             end
             local points = rng.RollInt(skillRange[1], skillRange[2])
             sTable.rating = sTable.rating < points and points or sTable.rating
             sTable.rating = sTable.rating > 20 and 20 or sTable.rating
+            sTable.rating = sTable.rating < 0 and 0 or sTable.rating
             if args.debug and tonumber(args.debug) >= 2 then print(skill .. ".rating = " .. sTable.rating) end
         end
     end
@@ -232,6 +323,7 @@ function ApplyProfession(dwf, profession, incr, min, max)
     for skill, bonus in pairs(prof.skills) do
         local sTable = GetSkillTable(dwf, skill)
         if sTable == nil then
+            --print("ApplyProfession()", profession, skill, bonus)
             utils.insert_or_update(dwf.status.current_soul.skills, { new = true, id = df.job_skill[skill], rating = 0 }, 'id')
             sTable = GetSkillTable(dwf, skill)
         end
@@ -239,6 +331,7 @@ function ApplyProfession(dwf, profession, incr, min, max)
         sTable.rating = sTable.rating < points and points or sTable.rating
         sTable.rating = sTable.rating + bonus
         sTable.rating = sTable.rating > 20 and 20 or sTable.rating
+        sTable.rating = sTable.rating < 0 and 0 or sTable.rating
         if args.debug and tonumber(args.debug) >= 2 then print(skill .. ".rating = " .. sTable.rating) end
     end
 end
@@ -268,20 +361,31 @@ function ApplyJob(dwf, job) --job is a dorf_jobs.<job> table
         local prioLength = ArrayLength(job.prio)
         local prioCount = 0
         --> max= math.ceil( total / (2 or 3) )
-        local prioMax = math.ceil(prioLength / 2)
+        local prioMax = math.ceil(prioLength / 2) + rng.RollInt(0,1)
+        prioMax = prioMax > prioLength and prioLength or prioMax
         rng.ResetIndexRolls(jobName .. ".job.prio", prioLength)
         for i=1,prioLength do
             --> Select random profession
             local prof = GetRandomTableEntry(jobName .. ".job.prio", job.prio)
             -->> Check isValid
-            if isValidProfession(prof, 0.7) and (prioCount < prioMax) then
+            if job.cur and job.min and job.cur[1] < job.min[1] then
+                if (prioCount < prioMax) then
+                    prioCount = prioCount + 1
+                    --> Set Profession 2 if not already done (by #)
+                    if not bAlreadySetProf2 then
+                        bAlreadySetProf2 = true
+                        dwf.profession2 = df.profession[prof]
+                    end
+                    ApplyProfession(dwf, prof, 0.66, 7, 10)
+                end
+            elseif (prioCount < prioMax) and isValidProfession(prof, 0.66) then
                 prioCount = prioCount + 1
                 --> Set Profession 2 if not already done (by #)
                 if not bAlreadySetProf2 then
                     bAlreadySetProf2 = true
                     dwf.profession2 = df.profession[prof]
                 end
-                ApplyProfession(dwf, prof, 0.7, 7, 10)
+                ApplyProfession(dwf, prof, 0.66, 7, 10)
             end
         end
     end
@@ -289,13 +393,23 @@ function ApplyJob(dwf, job) --job is a dorf_jobs.<job> table
     -- Loop tertiary professions
     for prof, p in pairs(job) do
         if tonumber(p) then
-            if isValidProfession(prof, 0.3) then
+            if isValidProfession(prof, 0.22) then
                 --> proc probability
                 if rng.RollBool(p) then
-                    ApplyProfession(dwf, prof, 0.3, 1, 5)
+                    if not bAlreadySetProf2 then
+                        bAlreadySetProf2 = true
+                        dwf.profession2 = df.profession[prof]
+                    end
+                    ApplyProfession(dwf, prof, 0.22, 1, 5)
                 end
             end
         end
+    end
+    if not bAlreadySetProf2 then
+        dwf.profession2 = dwf.profession
+    end
+    if job.cur and job.min and job.cur[1] < job.min[1] then
+        job.cur[1] = job.cur[1] + 1
     end
 end
 
@@ -316,25 +430,72 @@ function RollStats(dwf, types)
     end
 end
 
+--Returns true if a job was found and applied, returns false otherwise
 function FindJob(dwf)
-    if dwf.custom_profession == "" then
-        local totalJobs = TableLength(dorf_tables.dorf_jobs)
-        rng.ResetIndexRolls("dorf jobs", totalJobs)
-        for i=1,totalJobs do
-            local job = GetRandomTableEntry("dorf jobs", dorf_tables.dorf_jobs)
-            if args.debug and tonumber(args.debug) >= 2 then print("FindJob() iteration #" .. i, FindValueKey(job)) end
-            if isValidJob(job) then
-                if args.debug and tonumber(args.debug) >= 1 then print("Found a job!") end
-                ApplyJob(dwf, job)
-                return true
+    if args.clearfirst then
+        if not isDwarfNamed(dwf) then
+            if args.clearfirst == 'all' then
+                ZeroDwarf(dwf)
+            elseif args.clearfirst == 'allcustom' and isDwarfCustom(dwf) then
+                ZeroDwarf(dwf)
+            elseif args.clearfirst == 'allpimped' and isDwarfPimped(dwf) then
+                ZeroDwarf(dwf)
+            elseif args.clearfirst == 'allunpimped' and isDwarfUnpimped(dwf) then
+                ZeroDwarf(dwf)
+            elseif args.clearfirst == 'allcustomunpimped' and isDwarfCustomUnpimped(dwf) then
+                ZeroDwarf(dwf)
+            elseif args.clearfirst == 'allprotected' and isDwarfProtected(dwf) then
+                ZeroDwarf(dwf)
+            elseif args.clearfirst == 'allunprotected' and isDwarfUnprotected(dwf) then
+                ZeroDwarf(dwf)
+            elseif 
+            args.clearfirst ~= 'allcustom' and 
+            args.clearfirst ~= 'allpimped' and 
+            args.clearfirst ~= 'allunpimped' and 
+            args.clearfirst ~= 'allcustomunpimped' and 
+            args.clearfirst ~= 'allprotected' and 
+            args.clearfirst ~= 'allunprotected'
+            then
+                error(":ERROR: Please use a valid argument for -clearfirst\n{all, selected, allcustom, allpimped, allunpimped, allcustomunpimped, allprotected, allunprotected")
             end
+        elseif args.clearfirst == 'allnamed' then
+            ZeroDwarf(dwf)
         end
-        error("No job found, that is bad?!")
+    end
+    if isDwarfProtected(dwf) then
+        --We must count jobs for pimped dwarves, their unionized after all!!   Pimps be pimpin'
+        if isDwarfPimped(dwf) then
+            local jobName = dwf.custom_profession
+            local job = dorf_tables.dorf_jobs[jobName]
+            if not job.cur then --todo remove me eventually
+                job.cur = {0}
+            end
+            job.cur[1] = job.cur[1] + 1
+            function increase_prof(p) if p then p.cur = p.cur + 1 end end
+            increase_prof(dorf_tables.professions[df.job_skill[dwf.profession]])
+            increase_prof(dorf_tables.professions[df.job_skill[dwf.profession2]])
+        end
         return false
     end
+
+    local totalJobs = TableLength(dorf_tables.dorf_jobs)
+    rng.ResetIndexRolls("dorf jobs", totalJobs)
+    for i=1,totalJobs do
+        local job = GetRandomTableEntry("dorf jobs", dorf_tables.dorf_jobs)
+        if args.debug and tonumber(args.debug) >= 2 then print("FindJob() iteration #" .. i, FindValueKey(job)) end
+        if isValidJob(job) then
+            if args.debug and tonumber(args.debug) >= 1 then print("Found a job!") end
+            ApplyJob(dwf, job)
+            pimped_count = pimped_count + 1
+            return true
+        end
+    end
+    print(":WARNING: No job found, that is bad?!")
+    return false
 end
 
 function ZeroDwarf(dwf)
+    zeroed_count = zeroed_count + 1
     LoopStatsTable(dwf.body.physical_attrs, function(attribute) attribute.value = 0 end)
     LoopStatsTable(dwf.status.current_soul.mental_attrs, function(attribute) attribute.value = 0 end)
     local count_max = count_this(df.job_skill)
@@ -342,7 +503,10 @@ function ZeroDwarf(dwf)
     for i=0, count_max do
         utils.erase_sorted_key(dwf.status.current_soul.skills, i, 'id')
     end
+    dfhack.units.setNickname(dwf, "")
     dwf.custom_profession = ""
+    dwf.profession = df.profession['DRUNK']
+    dwf.profession2 = df.profession['DRUNK']
 end
 
 function LoopDwarfCitizens(units, callback)
@@ -364,19 +528,22 @@ end
     'applyprofession',
     'applytype',
     'selected',
-    'cleardwarf',
-    'cleardwarves',
+    'clear', --selected --all --allcustom --allpimped --allunpimped --allcustomunpimped --allprotected --allunprotected
+    'clearfirst',       --all --allcustom --allpimped --allunpimped --allcustomunpimped --allprotected --allunprotected
+    'query',
     'debug'
 --]]
 
 local SelectedUnit = nil
 local ActiveUnits = df.global.world.units.active
 dwarf_count = 0
-
+pimped_count = 0
+zeroed_count = 0
 selection_count = 0
 local selection = nil
-if args.selected or args.cleardwarf or args.applyjob or args.applyprofession or args.applytype then
+if args.selected or (args.clear and args.clear == "selected") or args.applyjob or args.applyprofession or args.applytype then
     SelectedUnit = dfhack.gui.getSelectedUnit()
+    assert(SelectedUnit, "Error: you must select a unit")
     if args.selected or args.cleardwarf then
         selection = {}
         table.insert(selection, SelectedUnit)
@@ -387,36 +554,62 @@ end
 
 dwarf_count = LoopDwarfCitizens(ActiveUnits)
 selection_count = LoopDwarfCitizens(selection)
-print(dwarf_count .. " dwarves")
-print(selection_count .. " dorf(s) selected")
+print("Dwarf population: " .. dwarf_count)
+print("Selected: " .. selection_count)
 
-if not (args.cleardwarves or args.cleardwarf or args.applyjob or args.applyprofession or args.applytype) then
-    LoopDwarfCitizens(selection, FindJob)
-    print(selection_count .. " dorf(s) have been pimped.")
-elseif args.cleardwarves or args.cleardwarf then
-    LoopDwarfCitizens(selection, ZeroDwarf)
-    print(selection_count .. " dorf(s) have been reset to zero.")
-elseif dfhack.units.isCitizen(SelectedUnit) then
-    local dwf = SelectedUnit
-    if args.applyjob then
-        if dorf_tables.dorf_jobs[args.applyjob] then
-            ApplyJob(dwf, args.applyjob)
-        else
-            error("Invalid job: " .. args.applyjob)
+if args.clear then
+    if args.clear == 'allcustom' then
+        LoopDwarfCitizens(selection, function(dwf) if isDwarfCustom(dwf) and not isDwarfNamed(dwf) then ZeroDwarf(dwf) end end)
+    elseif args.clear == 'allpimped' then
+        LoopDwarfCitizens(selection, function(dwf) if isDwarfPimped(dwf) and not isDwarfNamed(dwf) then ZeroDwarf(dwf) end end)
+    elseif args.clear == 'allunpimped' then
+        LoopDwarfCitizens(selection, function(dwf) if isDwarfUnpimped(dwf) and not isDwarfNamed(dwf) then ZeroDwarf(dwf) end end)
+    elseif args.clear == 'allcustomunpimped' then
+        LoopDwarfCitizens(selection, function(dwf) if isDwarfCustomUnpimped(dwf) and not isDwarfNamed(dwf) then ZeroDwarf(dwf) end end)
+    elseif args.clear == 'allprotected' then
+        LoopDwarfCitizens(selection, function(dwf) if isDwarfProtected(dwf) and not isDwarfNamed(dwf) then ZeroDwarf(dwf) end end)
+    elseif args.clear == 'allunprotected' then
+        LoopDwarfCitizens(selection, function(dwf) if isDwarfUnprotected(dwf) and not isDwarfNamed(dwf) then ZeroDwarf(dwf) end end)
+    elseif args.clear == 'allnamed' then
+        LoopDwarfCitizens(selection, function(dwf) if isDwarfNamed(dwf) then ZeroDwarf(dwf) end end)
+    elseif args.clear == 'all' or args.clear == 'selected' then
+        LoopDwarfCitizens(selection, ZeroDwarf)
+    else
+        error(":ERROR: Please use a valid argument for -clear\n{all, selected, allcustom, allpimped, allunpimped, allcustomunpimped, allprotected, allunprotected")
+    end
+    print(zeroed_count .. " dorf(s) have been reset to zero.")
+else
+    if not (args.applyjob or args.applyprofession or args.applytype) then
+        print("\nPimping Dwarves..")
+        LoopDwarfCitizens(selection, FindJob)
+        print("Results\n---------")
+        print(zeroed_count .. " dorf(s) were reset to zero.")
+        print(pimped_count .. " dorf(s) were pimped out.")
+    elseif dfhack.units.isCitizen(SelectedUnit) then
+        local dwf = SelectedUnit
+        if args.applyjob then
+            if dorf_tables.dorf_jobs[args.applyjob] then
+                ApplyJob(dwf, args.applyjob)
+            else
+                error("Invalid job: " .. args.applyjob)
+            end
+        elseif args.applyprofession then
+            if dorf_tables.professions[args.applyprofession] then
+                ApplyProfession(dwf, args.applyprofession)
+            else
+                error("Invalid profession: " .. args.applyprofession)
+            end
+        elseif args.applytype then
+            if dorf_tables.dorf_types[args.applytype] then
+                ApplyType(dwf, args.applytype)
+            else
+                error("Invalid type: " .. args.applytype)
+            end
         end
-    elseif args.applyprofession then
-        if dorf_tables.professions[args.applyprofession] then
-            ApplyProfession(dwf, args.applyprofession)
-        else
-            error("Invalid profession: " .. args.applyprofession)
-        end
-    elseif args.applytype then
-        if dorf_tables.dorf_types[args.applytype] then
-            ApplyType(dwf, args.applytype)
-        else
-            error("Invalid type: " .. args.applytype)
-        end
+    else
+        error("You did something wrong. Where's the poop!")
     end
 end
 
 rng.BlastDistributions()
+print('\n')
