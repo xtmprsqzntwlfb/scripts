ec51168a00c7e69b0e9cfd776d50e5fc06ac33f6
lethosor <lethosor@gmail.com>
2017-12-18 23:16:24 -0500

    Add docs

--- gui/cp437-table.lua
+++ gui/cp437-table.lua
@@ -1,5 +1,16 @@
 -- An in-game CP437 table
 
+--[====[
+
+gui/cp437-table
+===============
+
+An in-game CP437 table. Allows typing or selecting characters with the mouse.
+Input is fed directly to the parent screen when "enter" is pressed, so there
+should be a text field selected before running this script.
+
+]====]
+
 local gui = require 'gui'
 local dialog = require 'gui.dialogs'
 local gps = df.global.gps
