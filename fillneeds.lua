cefdd5542185d157d161b60e2ece21d93d2a8fc9
lethosor <lethosor@gmail.com>
2018-02-03 16:38:24 -0500

    fillneeds: improve docs, style

--- fillneeds.lua
+++ fillneeds.lua
@@ -1,23 +1,25 @@
---Use with a unit selected to make them focused and unstressed.
+-- Use with a unit selected to make them focused and unstressed.
 --[====[
 
 fillneeds
 =========
-Like it says on the tin, it fills your focus level all the way up and destresses as well.
+Use with a unit selected to make them focused and unstressed.
+
+Alternatively, a unit can be specified by passing ``-unit UNIT_ID``
 
 ]====]
-local utils=require('utils')
-local args = utils.processArgs({...}, validArgs)
+local utils = require('utils')
+local args = utils.processArgs({...})
 
 local unit = args.unit and df.unit.find(args.unit) or dfhack.gui.getSelectedUnit(true)
 
 if not unit then qerror('A unit must be specified or selected.') end
 
-function satisfyNeeds(unit,mind)
-local mind = unit.status.current_soul.personality.needs
+function satisfyNeeds(unit)
+    local mind = unit.status.current_soul.personality.needs
     for k,v in ipairs(mind) do
         mind[k].focus_level = 400
     end
-        unit.status.current_soul.personality.stress_level=-1000000
+    unit.status.current_soul.personality.stress_level = -1000000
 end
-satisfyNeeds(unit,mind)
+satisfyNeeds(unit)
