8d079a59122d9ba72ce9c0f7687402a343d09bc7
lethosor <lethosor@gmail.com>
2018-02-03 17:44:45 -0500

    questport: remove redundant screen type check

--- questport.lua
+++ questport.lua
@@ -14,31 +14,30 @@ local qp = dfhack.gui.getViewscreenByType(df.viewscreen_dungeonmodest, 0)
 local qmap = dfhack.gui.getViewscreenByType(df.viewscreen_adventure_logst, 0)
     or qerror("Could not find quest log screen")
 local qarm = df.global.world.armies.all
-if df.viewscreen_adventure_logst:is_instance(qmap) then
-    local qx = qmap.cursor_x * 48
-    local qy = qmap.cursor_y * 48
-    local rx = qmap.player_region_x * 48
-    local ry = qmap.player_region_y * 48
-    df.global.ui_advmode.unk_1 = qx
-    df.global.ui_advmode.unk_2 = qy
-    if df.global.ui_advmode.menu == df.ui_advmode_menu.Default then
-        gui.simulateInput(qp.child, 'LEAVESCREEN')
-        df.global.ui_advmode.menu = df.ui_advmode_menu.Travel
-        df.global.ui_advmode.travel_not_moved = true
-        gui.simulateInput(qp, 'CURSOR_DOWN')
-        dfhack.timeout(15, 'frames', function()
-            gui.simulateInput(qp, 'A_TRAVEL_LOG')
-        end)
-    elseif df.global.ui_advmode.menu == df.ui_advmode_menu.Travel then
-        for k,v in ipairs(qarm) do
-            if v.flags.player then
-                local my_arm = v.pos
-                if rx ~= qx or ry ~= qy then
-                    my_arm.x = qx
-                    my_arm.y = qy
-                    qmap.player_region_x = qmap.cursor_x
-                    qmap.player_region_y = qmap.cursor_y
-                end
+
+local qx = qmap.cursor_x * 48
+local qy = qmap.cursor_y * 48
+local rx = qmap.player_region_x * 48
+local ry = qmap.player_region_y * 48
+df.global.ui_advmode.unk_1 = qx
+df.global.ui_advmode.unk_2 = qy
+if df.global.ui_advmode.menu == df.ui_advmode_menu.Default then
+    gui.simulateInput(qp.child, 'LEAVESCREEN')
+    df.global.ui_advmode.menu = df.ui_advmode_menu.Travel
+    df.global.ui_advmode.travel_not_moved = true
+    gui.simulateInput(qp, 'CURSOR_DOWN')
+    dfhack.timeout(15, 'frames', function()
+        gui.simulateInput(qp, 'A_TRAVEL_LOG')
+    end)
+elseif df.global.ui_advmode.menu == df.ui_advmode_menu.Travel then
+    for k,v in ipairs(qarm) do
+        if v.flags.player then
+            local my_arm = v.pos
+            if rx ~= qx or ry ~= qy then
+                my_arm.x = qx
+                my_arm.y = qy
+                qmap.player_region_x = qmap.cursor_x
+                qmap.player_region_y = qmap.cursor_y
             end
         end
     end
