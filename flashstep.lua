7a07b13c4c0aeea599b1dbc138b5fb3738a98097
lethosor <lethosor@gmail.com>
2018-02-03 17:16:23 -0500

    flashstep: use teleport, clarify docs

--- flashstep.lua
+++ flashstep.lua
@@ -1,27 +1,21 @@
---Teleports unit to cursor
+--Teleports adventurer to cursor
 --[====[
 
 flashstep
 =========
-A hotkey-friendly teleport that places you where your cursor is.
+A hotkey-friendly teleport that places your adventurer where your cursor is.
 
 ]====]
-function flashstep(flash,step)
-    local flash = df.global.world.units.active[0]
-    local step
-        if df.global.ui_advmode.menu == df.ui_advmode_menu.Look then
-            step = df.global.cursor
-        else
-            qerror("No [l] cursor located! You kinda need it for this script.")
-        end
-    local unitoccupancy = dfhack.maps.getTileBlock(flash.pos).occupancy[flash.pos.x%16][flash.pos.y%16]
-        flash.pos.x = step.x
-        flash.pos.y = step.y
-        flash.pos.z = step.z
-            if flash.pos.x==step.x then
-            local stepvisibility = dfhack.maps.getTileBlock(flash.pos).designation[flash.pos.x%16][flash.pos.y%16]
-                stepvisibility.hidden=false
-            end
-    if not flash.flags1.on_ground then unitoccupancy.unit = false else unitoccupancy.unit_grounded = false end
+
+teleport = reqscript("teleport").teleport
+
+function flashstep()
+    local unit = df.global.world.units.active[0]
+    if df.global.ui_advmode.menu ~= df.ui_advmode_menu.Look then
+        qerror("No [l] cursor located! You kinda need it for this script.")
+    end
+    teleport(unit, xyz2pos(pos2xyz(df.global.cursor)))
+    dfhack.maps.getTileBlock(unit.pos).designation[unit.pos.x%16][unit.pos.y%16].hidden = false
 end
-flashstep(flash,step)
+
+flashstep()
