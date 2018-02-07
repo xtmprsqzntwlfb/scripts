4905a0719dd7dbe8dce53c9b988f0d96e2d0ad92
lethosor <lethosor@gmail.com>
2018-02-06 19:13:18 -0500

    adv-fix-sleepers: fix link, simplify a bit
    Ref #46

--- adv-fix-sleepers.lua
+++ adv-fix-sleepers.lua
@@ -3,10 +3,10 @@
 
 adv-fix-sleepers
 ================
-Fixes :bug:'6798'. This bug is characterized by sleeping units who refuse to
+Fixes :bug:`6798`. This bug is characterized by sleeping units who refuse to
     awaken in adventure mode regardless of talking to them, hitting them, or
     waiting so long you die of thirst.
-    
+
 Usage: If you come accross one or more bugged sleepers in adventure mode,
     simply run the script (type adv-fix-sleepers into the dfhack console),
     and all nearby sleepers will be cured.
@@ -25,15 +25,12 @@ local active_units = df.global.world.units.active -- get all active units
 local num_fixed = 0 -- this is the number of army controllers fixed, not units
     -- I've found that often, multiple sleepers share a bugged army controller
 for k, unit in pairs(active_units) do
-    if unit ~= nil then
-        local army_controller
-        army_controller = df.army_controller.find(unit.enemy.army_controller_id)
-        if not army_controller then
-            -- print ("no army_controller found matching given id")
-        elseif army_controller.type == 4 then -- sleeping code is possible
+    if unit then
+        local army_controller = df.army_controller.find(unit.enemy.army_controller_id)
+        if army_controller and army_controller.type == 4 then -- sleeping code is possible
             if army_controller.unk_64.t4.unk_2.not_sleeping == false then
                 army_controller.unk_64.t4.unk_2.not_sleeping = true -- fix bug
-                num_fixed = num_fixed + 1;
+                num_fixed = num_fixed + 1
             end
         end
     end
@@ -42,5 +39,5 @@ end
 if num_fixed == 0 then
     print ("No sleepers with the fixable bug were found, sorry.")
 else
-    print ("Fixed " .. num_fixed .. " bugged army_controllers.")
+    print ("Fixed " .. num_fixed .. " bugged army_controller(s).")
 end
