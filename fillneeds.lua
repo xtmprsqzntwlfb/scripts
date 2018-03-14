cd8e1d747e081ba90f3c9a2335d3283289bb67b6
lethosor <lethosor@gmail.com>
2018-03-14 14:35:35 -0400

    fillneeds: document -all option

--- fillneeds.lua
+++ fillneeds.lua
@@ -7,6 +7,8 @@ Use with a unit selected to make them focused and unstressed.
 
 Alternatively, a unit can be specified by passing ``-unit UNIT_ID``
 
+Use ``-all`` to apply to all units on the map.
+
 ]====]
 local utils = require('utils')
 local args = utils.processArgs({...})
