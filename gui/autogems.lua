7d4bf819e33ebf708617e31f49260550108b700a
lethosor <lethosor@gmail.com>
2018-05-04 23:32:30 -0400

    gui/autogems: Add documentation

--- gui/autogems.lua
+++ gui/autogems.lua
@@ -3,7 +3,28 @@
 gui/autogems
 ============
 
+A frontend for the `autogems` plugin that allows configuring the gem types to be cut.
 
+The following controls apply to the gems currently listed:
+
+- ``s``: Searches for matching gems
+- ``Shift+Enter``: Toggles the status of all listed gems
+
+The following controls apply to the gems currenty listed, as well as gems listed
+*before* the current search with ``s``, if applicable:
+
+- ``r``: Displays only "rock crystal" gems
+- ``c``: Displays only gems whose color matches the selected gem
+- ``m``: Displays only gems where at least one rough (uncut) gem is available
+  somewhere on the map
+
+This behavior is intended to allow for things like a search for "lazuli"
+followed by pressing ``c`` to select all gems with the same color as lapis
+lazuli (5 blue gems in vanilla DF), rather than further restricting that to gems
+with "lazuli" in their name (only 1).
+
+``x`` clears all filters, which is currently the only way to undo filters
+(besides searching), and is useful to verify the gems selected.
 
 ]====]
 
