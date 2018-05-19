bef9b5c84f2e373b481c149733f16353e556ecd8
grubsteak <joel.herzog@att.net>
2018-05-19 15:00:39 -0500

    fix title
    travis is a bastard

--- gui/stamper.lua
+++ gui/stamper.lua
@@ -2,9 +2,9 @@
 
 --[====[
 
-stamper
-=======
-stamper that allows manipulation of designations by transforms such as translations, reflections, rotations, and inversion.
+gui/stamper
+===========
+allows manipulation of designations by transforms such as translations, reflections, rotations, and inversion.
 designations can also be used as brushes to erase other designations and cancel constructions.
 
 ]====]
