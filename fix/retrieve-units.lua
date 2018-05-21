27ea437cd9ccaa316d40df6a399f051838bec7d8
lethosor <lethosor@gmail.com>
2018-05-21 17:45:47 -0400

    fix/retrieve-units: also add units back to active list to counteract fix/dead-units

--- fix/retrieve-units.lua
+++ fix/retrieve-units.lua
@@ -23,9 +23,22 @@ such as the following:
 
 ]====]
 
+utils = require('utils')
+
+function shouldRetrieve(unit)
+    if unit.flags1.incoming then
+        return true
+    elseif (unit.flags1.merchant or unit.flags1.invades) and not (unit.flags2.killed or unit.flags2.slaughter) then
+        -- killed/slaughter check from http://www.bay12games.com/dwarves/mantisbt/view.php?id=10075#c38332
+        return true
+    else
+        return false
+    end
+end
+
 function retrieveUnits()
-    for _, unit in pairs(df.global.world.units.active) do
-        if unit.flags1.dead and unit.flags1.incoming then
+    for _, unit in pairs(df.global.world.units.all) do
+        if unit.flags1.dead and shouldRetrieve(unit) then
             print(("Retrieving from the abyss: %s (%s)"):format(
                 dfhack.df2console(dfhack.TranslateName(dfhack.units.getVisibleName(unit))),
                 df.creature_raw.find(unit.race).name[0]
@@ -35,6 +48,8 @@ function retrieveUnits()
             unit.flags1.incoming = false
             unit.flags1.can_swap = true
             unit.flags1.hidden_in_ambush = false
+            -- add to active if missing
+            utils.insert_sorted(df.global.world.units.active, unit, 'id')
         end
     end
 end
