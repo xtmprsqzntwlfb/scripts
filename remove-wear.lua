3e7806c3e29b5124298dec42890c925f8159489a
lethosor <lethosor@gmail.com>
2018-06-30 00:52:17 -0400

    remove-wear: fix indentation

--- remove-wear.lua
+++ remove-wear.lua
@@ -19,41 +19,41 @@ if args[1] == 'help' then
     print(help)
     return
 elseif args[1] == 'all' then
- local count = 0;
- for _,item in ipairs(df.global.world.items.all) do
-  if (item.wear > 0) then --hint:df.item_actual
-   item:setWear(0)
-   count = count+1
-  end
- end
- print('remove-wear removed wear from '..count..' objects')
+    local count = 0;
+    for _,item in ipairs(df.global.world.items.all) do
+        if (item.wear > 0) then --hint:df.item_actual
+            item:setWear(0)
+            count = count+1
+        end
+    end
+    print('remove-wear removed wear from '..count..' objects')
 else
- local argIndex = 1
- local isCompleted = {} --as:bool[]
- for i,x in ipairs(args) do
-  args[i] = tonumber(x)
- end
- table.sort(args)
- for _,item in ipairs(df.global.world.items.all) do
-  local function loop()
-   if argIndex > #args then
-    return
-   elseif item.id > args[argIndex] then
-    argIndex = argIndex+1
-    loop()
-    return
-   elseif item.id == args[argIndex] then
-    --print('removing wear from item with id ' .. args[argIndex])
-    item:setWear(0)
-    isCompleted[args[argIndex]] = true
-    argIndex = argIndex+1
-   end
-  end
-  loop()
- end
- for _,arg in ipairs(args) do
-  if isCompleted[arg] ~= true then
-   print('failed to remove wear from item ' .. arg .. ': could not find item with that id')
-  end
- end
+    local argIndex = 1
+    local isCompleted = {} --as:bool[]
+    for i,x in ipairs(args) do
+        args[i] = tonumber(x)
+    end
+    table.sort(args)
+    for _,item in ipairs(df.global.world.items.all) do
+        local function loop()
+            if argIndex > #args then
+                return
+            elseif item.id > args[argIndex] then
+                argIndex = argIndex+1
+                loop()
+                return
+            elseif item.id == args[argIndex] then
+                --print('removing wear from item with id ' .. args[argIndex])
+                item:setWear(0)
+                isCompleted[args[argIndex]] = true
+                argIndex = argIndex+1
+            end
+        end
+        loop()
+    end
+    for _,arg in ipairs(args) do
+        if isCompleted[arg] ~= true then
+            print('failed to remove wear from item ' .. arg .. ': could not find item with that id')
+        end
+    end
 end
