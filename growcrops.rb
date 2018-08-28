1727e34203a3a484dbcfe489b77e45eb2f3bf2b8
Matt Regul <mattregul@users.noreply.github.com>
2018-08-28 11:26:38 -0400

    Added growcrops all
    - Added support for "all", growing all planted crops
    - Split code up for easy-to-follow flow
    - Commented code

--- growcrops.rb
+++ growcrops.rb
@@ -9,56 +9,104 @@ With no argument, this command list the various seed types currently in
 use in your farming plots. With a seed type, the script will grow 100 of
 these seeds, ready to be harvested. Set the number with a 2nd argument.
 
-For example, to grow 40 plump helmet spawn::
+Arguments:
 
-    growcrops plump 40
+- ``list`` or ``help``
+   List the various seed types currently in use in your farming plots
+
+- ``<crop name>``
+   Grow specific planted seed type
+
+- ``all``
+   Grow all planted seeds
+
+
+Example:
+
+- Grow 40 plump helmet spawn:
+   ``growcrops plump 40``
 
 =end
 
-material = $script_args[0]
-count_max = $script_args[1].to_i
-count_max = 100 if count_max == 0
-
 # cache information from the raws
-@raws_plant_name ||= {}
-@raws_plant_growdur ||= {}
-if @raws_plant_name.empty?
-    df.world.raws.plants.all.each_with_index { |p, idx|
-        @raws_plant_name[idx] = p.id
-        @raws_plant_growdur[idx] = p.growdur
+def cacheCropRaws()
+    if @raws_crop_name.empty?
+        # store ID and grow duration for each crop
+        df.world.raws.plants.all.each_with_index { |p, idx|
+            @raws_crop_name[idx] = p.id # Crop ID
+            @raws_crop_growdur[idx] = p.growdur # Grow Duration
+        }
+    end
+end 
+
+# create a list of available crops to grow, from seeds
+def buildSeedList()
+    df.world.items.other[:SEEDS].each { |seed|
+        next if not seed.flags.in_building # skip if seed in building
+        next if not seed.general_refs.find { |ref| ref._rtti_classname == :general_ref_building_holderst } # skip if seed in depot
+        next if seed.grow_counter >= @raws_crop_growdur[seed.mat_index]
+        
+        # add to list of potential crops to grow
+        @inventory[seed.mat_index] += 1
     }
 end
 
-inventory = Hash.new(0)
-df.world.items.other[:SEEDS].each { |seed|
-    next if not seed.flags.in_building
-    next if not seed.general_refs.find { |ref| ref._rtti_classname == :general_ref_building_holderst }
-    next if seed.grow_counter >= @raws_plant_growdur[seed.mat_index]
-    inventory[seed.mat_index] += 1
-}
-
-if !material or material == 'help' or material == 'list'
-    # show a list of available crop types
-    inventory.sort_by { |mat, c| c }.each { |mat, c|
-        name = df.world.raws.plants.all[mat].id
-        puts " #{name} #{c}"
+# Display a list of planted crops
+def listPlantedCrops()
+    @inventory.sort_by { |mat, seedCount| seedCount }.each { |mat, seedCount|
+        cropName = df.world.raws.plants.all[mat].id
+        puts " #{cropName} #{seedCount}"
     }
+end
 
-else
-
-    mat = df.match_rawname(material, inventory.keys.map { |k| @raws_plant_name[k] })
-    unless wantmat = @raws_plant_name.index(mat)
-        raise "invalid plant material #{material}"
+# grow specific crop
+def growCrop(material)
+    # find the matching crop
+    mat = df.match_rawname(material, @inventory.keys.map { |k| @raws_crop_name[k] })
+    unless wantmat = @raws_crop_name.index(mat)
+        raise "invalid plant material #{material}" # no crop with that name
     end
-
+    # grow each seed for specified crop
     count = 0
     df.world.items.other[:SEEDS].each { |seed|
-        next if seed.mat_index != wantmat
-        next if not seed.flags.in_building
-        next if not seed.general_refs.find { |ref| ref._rtti_classname == :general_ref_building_holderst }
-        next if seed.grow_counter >= @raws_plant_growdur[seed.mat_index]
-        seed.grow_counter = @raws_plant_growdur[seed.mat_index]
+        next if seed.mat_index != wantmat # skip if not desired seed
+        next if not seed.flags.in_building # skip if seed is in building
+        next if not seed.general_refs.find { |ref| ref._rtti_classname == :general_ref_building_holderst } # skip if seed in depot
+        next if seed.grow_counter >= @raws_crop_growdur[seed.mat_index] # skip if already grew desired amount
+        seed.grow_counter = @raws_crop_growdur[seed.mat_index]
         count += 1
     }
     puts "Grown #{count} #{mat}"
+end
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+material = $script_args[0]          # (1st arg) Grow which crop
+count_max = $script_args[1].to_i    # (2nd arg) Maximum to grow
+count_max = 100 if count_max == 0   # if max wasn't supplied, grow up to 100 crops
+@inventory = Hash.new(0)            # a list of available crops to grow, from seeds
+@raws_crop_name ||= {}
+@raws_crop_growdur ||= {}
+
+# cache information from the raws
+cacheCropRaws()
+
+# create a list of available crops to grow, from seeds
+buildSeedList()
+
+if !material or material == 'help' or material == 'list'
+    # show a list of available crop types
+    listPlantedCrops()
+else
+    if material == 'all'
+        # loop through all planted crops
+        @inventory.sort_by { |mat, seedCount| seedCount }.each { |mat, seedCount|
+            # grow that plant
+            growCrop(df.world.raws.plants.all[mat].id)
+        }
+    else
+        # grow a single crop type
+        growCrop(material)
+    end
 end
