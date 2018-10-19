19e195c527c7dbe58616dda37384dc338f0cc98e
Josh Cooper <cooper.s.josh@outlook.com>
2018-10-19 08:25:51 -0700

    Makes query.lua more user friendly

--- query.lua
+++ query.lua
@@ -1,3 +1,37 @@
+-- Query is a script useful for finding and reading values of data structure fields. Purposes will likely be exclusive to writing lua script code.
+-- Written by Josh Cooper(cppcooper) on 2017-12-21, last modified: 2018-10-19
+local utils=require('utils')
+local validArgs = utils.invert({
+ 'unit',
+ 'table',
+ 'query'
+})
+local args = utils.processArgs({...}, validArgs)
+local help = [====[
+query
+=====
+Query is a script useful for finding and reading values of data structure fields.
+Purposes will likely be exclusive to writing lua script code.
+
+This script will recursively search an input for fields matching your query.
+Matching fields will be printed along with their values. When a match is a
+table the entire table will not be printed, only the reference to it.
+
+When performing table queries, use dot notation to denote sub-tables.
+The script has to parse the input string and separate each table.
+
+example: "query -unit -query profession" will print all fields matching
+'*profession*' inside the highlighted unit.
+
+The values input to -unit or -table must be global identifiers
+(ie. _G[identifier] must exist).
+
+Usage: ``query -unit <global_value> -query <query>``
+or ``query -table <global_value> -query <query>``
+
+]====]
+
+--thanks goes mostly to the internet for this function. thanks internet you da real mvp
 function safe_pairs(item, keys_only)
     if keys_only then
         local mt = debug.getmetatable(item)
@@ -28,7 +62,7 @@ function Query(table, query, parent)
     for k,v in safe_pairs(table) do
         if not tonumber(k) and type(k) ~= "table" and not string.find(tostring(k), 'script') then
             if string.find(tostring(k), query) then
-                print(parent .. "." .. k)
+                print(parent .. "." .. k .. ":", v)
             end
             --print(parent .. "." .. k)
             if not string.find(parent, tostring(k)) then
@@ -41,10 +75,54 @@ function Query(table, query, parent)
         end
     end
 end
-local selectedUnit = dfhack.gui.getSelectedUnit()
-Query(selectedUnit, 'level','selected-unit')
-print(selectedUnit.flags1.tame)
-print(selectedUnit.training_level)
---print(selectedUnit.following.flags1.tame)
---print(selectedUnit.job.hunt_target.flags1.tame)
---Query(PimpData, '', 'pd')
+
+function parseTableString(str)
+    tableParts = {}
+    for word in string.gmatch(str, '([^.]+)') do --thanks stack overflow
+        table.insert(tableParts, word)
+    end
+    curTable = nil
+    for k,v in pairs(tableParts) do
+      if curTable == nil then
+        if _G[v] ~= nil then
+            curTable = _G[v]
+        else
+            qerror("Table" .. v .. " does not exist.")
+        end
+      else
+        if curTable[v] ~= nil then
+            curTable = curTable[v]
+        else
+            qerror("Table" .. v .. " does not exist.")
+        end
+      end
+    end
+    return curTable
+end
+
+local selection = nil
+if args.unit then
+    if _G[args.unit] ~= nil then
+        selection = _G[args.unit]
+    else
+        selection = dfhack.gui.getSelectedUnit()
+    end
+    if selection == nil then
+        qerror("Selected unit is null. Invalid selection.")
+    elseif args.query ~= nil then
+        Query(selection, args.query, 'selected-unit')
+    else
+        print("The query is empty, the output is probably gonna be large. Start your engines.")
+        Query(selection, '', 'selected-unit')
+    end
+elseif args.table then
+    local t = parseTableString(args.table)
+    if args.query ~= nil then
+        Query(_G[args.table], args.query, args.table)
+    else
+        print("The query is empty, the output is probably gonna be large. Start your engines.")
+        Query(_G[args.table], '', args.table)
+    end
+else
+    print(help)
+end
