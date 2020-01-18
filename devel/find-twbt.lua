9d1c385092b84a30468a2ee400bef09bd84d5493
lethosor <lethosor@gmail.com>
2020-01-17 23:43:23 -0500

    tabs -> spaces

--- devel/find-twbt.lua
+++ devel/find-twbt.lua
@@ -11,14 +11,14 @@ local ms = require('memscan')
 local cs = ms.get_code_segment()
 
 function get_ptr_size()
-	local v = df.new('uintptr_t')
-	local ret = df.sizeof(v)
-	df.delete(v)
-	return ret
+    local v = df.new('uintptr_t')
+    local ret = df.sizeof(v)
+    df.delete(v)
+    return ret
 end
 
 function print_off(name, off)
-	print(string.format("<global-address name='%s' value='0x%x'/>", name, off - dfhack.internal.getRebaseDelta()))
+    print(string.format("<global-address name='%s' value='0x%x'/>", name, off - dfhack.internal.getRebaseDelta()))
 end
 
 local ptr_size = get_ptr_size()
@@ -32,35 +32,35 @@ local render_method = vtable[2] --third method aka render
 -- print_off("render", render_method)
 
 function list_all_possible_calls(start, len)
-	local func = ms.CheckedArray.new('uint8_t', start, start + len) -- should be near
-	local possible_calls = {}
+    local func = ms.CheckedArray.new('uint8_t', start, start + len) -- should be near
+    local possible_calls = {}
 
-	for i = 0, #func - 1 do
-		if func[i] == 0xe8 then
-			table.insert(possible_calls, i)
-		end
-	end
-	return possible_calls
+    for i = 0, #func - 1 do
+        if func[i] == 0xe8 then
+            table.insert(possible_calls, i)
+        end
+    end
+    return possible_calls
 end
 function get_call_target(offset)
-	local call_offset = df.reinterpret_cast('int32_t', offset + 1)
-	local ret = call_offset.value + offset + 5
-	if cs:contains_range(ret, 1) then
-		return ret
-	else
-		return nil
-	end
+    local call_offset = df.reinterpret_cast('int32_t', offset + 1)
+    local ret = call_offset.value + offset + 5
+    if cs:contains_range(ret, 1) then
+        return ret
+    else
+        return nil
+    end
 end
 function list_all_valid_calls(start, len)
-	local all_calls = list_all_possible_calls(start, len)
-	local ret = {}
-	for i, v in ipairs(all_calls) do
-		local ct = get_call_target(start + v)
-		if ct then
-			table.insert(ret, ct)
-		end
-	end
-	return ret
+    local all_calls = list_all_possible_calls(start, len)
+    local ret = {}
+    for i, v in ipairs(all_calls) do
+        local ct = get_call_target(start + v)
+        if ct then
+            table.insert(ret, ct)
+        end
+    end
+    return ret
 end
 -- TODO(warmist): this is probably stupid, but without disassembler ( sad face ) i can't say for sure
 -- if it's part of instruction or an arg, so we just hope to find only one?
@@ -68,9 +68,9 @@ end
 local possible_calls = list_all_valid_calls(render_method, 50)
 
 if #possible_calls == 0 then
-	qerror("Failed to find call instruction in render vmethod")
+    qerror("Failed to find call instruction in render vmethod")
 elseif #possible_calls > 1 then
-	qerror("Found multiple 0xe8 (call) in render vmethod start")
+    qerror("Found multiple 0xe8 (call) in render vmethod start")
 end
 
 local dwarfmode_render_main = possible_calls[1]
