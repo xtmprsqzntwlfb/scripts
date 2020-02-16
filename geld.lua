f10b1d82d8d9100eef39fa7337411e4e0e4e92f0
Josh Cooper <cooper.s.josh@outlook.com>
2020-02-15 21:06:01 -0800

    Adds gelding scripts

---
+++ geld.lua
@@ -0,0 +1,31 @@
+-- Written by Josh Cooper(cppcooper) on 2019-12-10, last modified: 2020-02-15
+utils ={}
+utils = require('utils')
+local validArgs = utils.invert({
+    'toggle',
+    'ungeld'
+})
+local args = utils.processArgs({...}, validArgs)
+
+unit = dfhack.gui.getSelectedUnit()
+oldstate = unit.flags3.gelded
+
+if args.toggle then
+    newstate = not oldstate
+    unit.flags3.gelded = newstate
+    print(string.format("gelded: %s => %s\n", state(oldstate), state(newstate)))
+elseif args.ungeld then
+    unit.flags3.gelded = false
+    print("unit ungelded.")
+else
+    unit.flags3.gelded = true
+    print("unit gelded.")
+end
+
+function state(st)
+    if st then
+        return "true"
+    else
+        return "false"
+    end
+end
---
+++ ungeld.lua
@@ -0,0 +1,4 @@
+-- Written by Josh Cooper(cppcooper) on 2019-12-10, last modified: 2019-12-10
+unit = dfhack.gui.getSelectedUnit()
+unit.flags3.gelded = false
+print("unit ungelded.")
