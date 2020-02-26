601331dca0a983ffab118382c3257ba084929230
Timur Kelman <tymur.gubayev@gmail.com>
2020-02-26 17:10:07 +0100

    rename script

--- on-new-fortress.lua
+++ on-new-fortress.lua
@@ -2,13 +2,13 @@
 
 local HELP = [====[
 
-onNewFortress
-=============
+on-new-fortress
+===============
 Runs commands like ``multicmd``, but only in a newborn fortress.
 
 Use this in ``onMapLoad.init`` with f.e. ``ban-cooking``::
 
-  onNewFortress ban-cooking tallow; ban-cooking honey; ban-cooking oil; ban-cooking seeds; ban-cooking brew; ban-cooking fruit; ban-cooking mill; ban-cooking thread; ban-cooking milk;
+  on-new-fortress ban-cooking tallow; ban-cooking honey; ban-cooking oil; ban-cooking seeds; ban-cooking brew; ban-cooking fruit; ban-cooking mill; ban-cooking thread; ban-cooking milk;
 
 ]====]
 
