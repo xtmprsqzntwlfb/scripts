bca04fbde41af92927e2767535c2b727b46c3ce0
PatrikLundell <j.patrik.r.lundell@gmail.com>
2020-03-02 19:41:51 +0100

    added more comments

--- lightaqonly.lua
+++ lightaqonly.lua
@@ -8,6 +8,11 @@ a value that results in Light aquifers instead.
 
 Note that the script has to be run before embarking to have any effect on an embark.
 
+This script is based on logic revealed by Toady in a FotF answer:
+http://www.bay12forums.com/smf/index.php?topic=169696.msg8099138#msg8099138
+Basically the Drainage is used as an "RNG" to cause an aquifer to be heavy
+about 5% of the time. The script shifts the matching numbers to a neighboring
+one, which does not result in any change of the biome.
 ]====]
 function lightaqonly ()
   if not dfhack.isWorldLoaded () then
