d61974cf357150f8da162f6427631d0988467c45
Timur Kelman <tymur.gubayev@gmail.com>
2020-03-09 09:34:04 +0100

    add `3dveins` as usage example

--- on-new-fortress.lua
+++ on-new-fortress.lua
@@ -9,6 +9,7 @@ Runs commands like `multicmd`, but only in a newborn fortress.
 Use this in ``onMapLoad.init`` with f.e. `ban-cooking`::
 
   on-new-fortress ban-cooking tallow; ban-cooking honey; ban-cooking oil; ban-cooking seeds; ban-cooking brew; ban-cooking fruit; ban-cooking mill; ban-cooking thread; ban-cooking milk;
+  on-new-fortress 3dveins
 
 ]====]
 
