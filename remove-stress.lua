62ae6559a01ed23e97380feb1e3857301b7ab2fb
Ryan Williams <Bumber64@users.noreply.github.com>
2020-05-12 22:11:24 -0700

    No longer uses array for value

--- remove-stress.lua
+++ remove-stress.lua
@@ -8,8 +8,8 @@ remove-stress
 Sets stress to -1,000,000; the normal range is 0 to 500,000 with very stable or
 very stressed dwarves taking on negative or greater values respectively.
 Applies to the selected unit, or use ``remove-stress -all`` to apply to all units.
-Using the argument ``-value [ 0 ]`` will reduce stress to the value 0 instead of -1,000,000.
-
+Using the argument ``-value 0`` will reduce stress to the value 0 instead of -1,000,000.
+Negative values must be preceded by a backslash (\): ``-value \-10000``
 ]====]
 
 local utils = require 'utils'
@@ -42,8 +42,8 @@ function main(...)
     end
     
     if args.value then
-        if args.value[1] then
-            stress_value = tonumber(args.value[1])
+        if tonumber(args.value) ~= nil then
+            stress_value = tonumber(args.value)
         else
             error 'Invalid usage: Missing stress value for -value.'
         end
