42134bee06ddb06e3edf5f41dcdd898fa087ff47
lethosor <lethosor@gmail.com>
2020-07-14 23:58:41 -0400

    Fix gm-editor docs (minor)

--- gui/gm-editor.lua
+++ gui/gm-editor.lua
@@ -5,15 +5,15 @@
 gui/gm-editor
 =============
 This editor allows to change and modify almost anything in df. Press :kbd:`?` for
-in-game help. There are three ways to open this editor:
+in-game help. There are multiple ways to open this editor:
 
 * Calling ``gui/gm-editor``  from a command or keybinding opens the editor
   on whatever is selected or viewed (e.g. unit/item description screen)
 
-* using gui/gm-editor <lua command> - executes lua command and opens editor on
+* using ``gui/gm-editor <lua command>`` - executes lua command and opens editor on
   its results (e.g. ``gui/gm-editor "df.global.world.items.all"`` shows all items)
 
-* using gui/gm-editor dialog - shows an in game dialog to input lua command. Works
+* using ``gui/gm-editor dialog`` - shows an in game dialog to input lua command. Works
   the same as version above.
 
 * using ``gui/gm-editor toggle`` - will hide (if shown) and show (if hidden) editor at
