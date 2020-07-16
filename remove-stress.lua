b921ee6b85100605ca83955f2f4720e4391ae444
lethosor <lethosor@gmail.com>
2020-07-15 22:13:53 -0400

    Clarify docs from #154

--- remove-stress.lua
+++ remove-stress.lua
@@ -9,8 +9,11 @@ remove-stress
 Sets stress to -1,000,000; the normal range is 0 to 500,000 with very stable or
 very stressed dwarves taking on negative or greater values respectively.
 Applies to the selected unit, or use ``remove-stress -all`` to apply to all units.
+
 Using the argument ``-value 0`` will reduce stress to the value 0 instead of -1,000,000.
-Negative values must be preceded by a backslash (\): ``-value \-10000``
+Negative values must be preceded by a backslash (\): ``-value \-10000``.
+Note that this can only be used to *decrease* stress - it cannot be increased
+with this argument.
 
 ]====]
 
@@ -42,7 +45,7 @@ function main(...)
         print(help)
         return
     end
-    
+
     if args.value then
         if tonumber(args.value) ~= nil then
             stress_value = tonumber(args.value)
