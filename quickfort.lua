b229d5ac091fdba833dde39e650b564bae070964
Myk Taylor <myk.taylor@gmail.com>
2020-07-18 06:24:52 -0700

    quickfort help text: initial draft

---
+++ quickfort.lua
@@ -0,0 +1,91 @@
+-- DFHack-native implementation of the classic Quickfort utility (PRE-RELEASE).
+--[====[
+
+quickfort
+=========
+Processes Quickfort-style blueprint files. This is a pre-release -- not all
+features are implemented yet.
+
+Quickfort blueprints record what you want at each map coordinate in a
+spreadsheet, storing the keys in a spreadsheet cell that you would press to make
+something happen at that spot on the DF map. Quickfort runs in one of four
+modes: ``dig``, ``build``, ``place``, or ``query``. ``dig`` designates tiles for
+digging, ``build`` builds buildings and constructions, ``place`` places
+stockpiles, and ``query`` changes building or stockpile settings. The mode is
+determined by a marker in the upper-left cell of the spreadsheet (e.g.: ``#dig``
+in cell ``A1``).
+
+You can create these blueprints by hand or by using any spreadsheet application,
+saving them as ``.xlsx`` or ``.csv`` files. You can also build your plan "for
+real" in Dwarf Fortress, and then export your map using the DFHack
+`blueprint plugin`_ for later replay in a different fort. Blueprint files should
+go in the ``blueprints`` subfolder in the main DF folder.
+
+You can see the full format specification for blueprint files, as well as
+ready-to-use examples of blueprints in each of the four modes in the
+``blueprints/library`` folder (or, of course, in the
+`DFHack source repository`_).
+
+Usage:
+
+**quickfort set [<key> <value>]**
+    Allows you to modify the active quickfort configuration. Just run
+    ``quickfort set`` to show current settings. See the Configuration section
+    below for available keys and values.
+**quickfort list [-l]**
+    Lists blueprints in the ``blueprints`` folder. Blueprints are ``.csv`` files
+    or sheets within ``.xlsx`` files that contain a ``#<mode>`` comment in the
+    upper-left cell. By default, blueprints in the ``blueprints/library/``
+    subfolder are not included. Specify ``-l`` to include library blueprints.
+**quickfort <command> <list_num>**
+    Applies the blueprint with the number from the list command.
+**quickfort <command> <filename> [-s <sheet_num>]**
+    Applies the blueprint from the named file. If it is an ``.xlsx`` file,
+    the ``-s`` parameter is required to identify the sheet number (the first
+    sheet is ``-s 1``).
+
+**<command>** can be one of:
+
+``run``
+    applies the blueprint at your current active cursor position.
+``orders``
+    uses the manager interface to queue up orders for the specified build-mode
+    blueprint.
+``undo``
+    applies the inverse of the specified blueprint, depending on its type. Dig
+    tiles are undesignated, buildings are canceled or removed (depending on
+    their construction status), and stockpiles are removed. No effect for query
+    blueprints.
+
+Configuration:
+
+The quickfort script reads its startup configuration from the
+``dfhack-config/quickfort/quickfort.txt`` file, which you can customize. The
+following settings may be dynamically modified by the ``quickfort set`` command
+(note that settings changed with the ``quickfort set`` command will not change
+the configuration stored in the file):
+
+``blueprints-dir`` (default: 'blueprints')
+    Can be set to an absolute or relative path. If set to a relative path,
+    resolves to a directory under the DF folder.
+``force-marker-mode`` (default: 'false')
+    Set to "true" or "false". If true, will designate dig blueprints in marker
+    mode. If false, only cells with dig codes prefixed with ``m`` will be
+    designated in marker mode.
+``interactive-build`` (default: 'false')
+    Allows you to manually select building materials for each
+    building/construction when running (or creating orders for) build
+    blueprints. Materials in selection dialogs are ordered according to
+    preferences in ``materials.txt`` (see below).
+
+There are also two other configuration files in the ``dfhack-config/quickfort``
+folder: ``aliases.txt`` and ``materials.txt``. ``aliases.txt`` defines keycode
+shortcuts for query blueprints, and ``materials.txt`` defines forbidden
+materials and material preferences for build blueprints. The formats for these
+files are described in the files themselves.
+
+.. _blueprint plugin: https://docs.dfhack.org/en/stable/docs/Plugins.html#blueprint
+.. _DFHack source repository: https://github.com/DFHack/dfhack/tree/develop/data/blueprints
+]====]
+
+print('Given arguments: ',...)
