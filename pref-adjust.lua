62a68ee385cb165d16c6a7f8ada6cbf6bb2e1ded
lethosor <lethosor@gmail.com>
2020-07-29 21:42:30 -0400

    Use quote formatting instead of code formatting for preference examples

--- pref-adjust.lua
+++ pref-adjust.lua
@@ -6,7 +6,7 @@ pref-adjust
 ===========
 ``pref-adjust all`` removes/changes preferences from all dwarves, and
 ``pref-adjust one`` which works for a single currently selected dwarf.
-For either, the script inserts an 'ideal' set which is easy to satisfy::
+For either, the script inserts an 'ideal' set which is easy to satisfy:
 
     ... likes iron, steel, weapons, armor, shields/bucklers and plump helmets
     for their rounded tops.  When possible, she prefers to consume dwarven
@@ -14,7 +14,7 @@ For either, the script inserts an 'ideal' set which is easy to satisfy::
     detests trolls, buzzards, vultures and crundles.
 
 Additionally, ``pref-adjust goth`` will insert a less than ideal set, which
-is quite challenging, for a single dwarf::
+is quite challenging, for a single dwarf:
 
     ... likes dwarf skin, corpses, body parts, remains, coffins, the color
     black, crosses, glumprongs for their living shadows and snow demons for
